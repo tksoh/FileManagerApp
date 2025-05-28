2a5bd94a349a5215fa0dc95d0c89f2f1a7c67272
TK Soh <teekaysoh@gmail.com>
2025-05-28 17:33:23 +0800

    fix bottom pixel overflow when displaying keyboard

--- lib/view/screens/home_screen.dart
+++ lib/view/screens/home_screen.dart
@@ -38,6 +38,7 @@ class _HomePageState extends State<HomePage> {
     return ControlBackButton(
       controller: myController.controller,
       child: Scaffold(
+        resizeToAvoidBottomInset: false,
         appBar: appBar(context),
         body: FileManager(
           controller: myController.controller,
